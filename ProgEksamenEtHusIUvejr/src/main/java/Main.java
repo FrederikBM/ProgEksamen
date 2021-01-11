8d12bc9c70a0d3cee3b06ffafcd66a2c921a7fea
FrederikBM <54077792+FrederikBM@users.noreply.github.com>
2021-01-11 09:44:53 +0100

    Add files via upload

---
+++ ProgEksamenEtHusIUvejr/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import processing.core.PApplet;
+import processing.core.PVector;
+import processing.data.StringList;
+
+import java.sql.*;
+
+public class Main extends PApplet{
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    public void settings() {
+        size(1000,2000);
+    }
+}
