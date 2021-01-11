c1e15004573df97f50e6e7c4bacaede0fcb0de1a
Frederik <frederikbrieghel@gmail.com>
2021-01-11 10:27:19 +0100

    Tilføjede databasen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,30 @@
-public class Main {
+import processing.core.PApplet;
+import processing.core.PVector;
+import processing.data.StringList;
+
+import java.sql.*;
+
+public class Main extends PApplet{
+    private final String databaseURL = "jdbc:ucanaccess://src//main//resources//Database.accdb";
+    private Connection connection = null;
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    public Main() {
+        try {
+            connection = DriverManager.getConnection(databaseURL);
+            println("Connected to MS Access database. ");
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void settings() {
+        size(500, 500);
+    }
+
 }
