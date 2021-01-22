357d88f27b5933f09203bfbaa84c6c9c055b51da
katrinesondrup <katrinesondrup@gmail.com>
2021-01-22 08:44:47 +0100

    hej

--- src/main/java/Windspeed.java
+++ src/main/java/Windspeed.java
@@ -1,2 +1,12 @@
-public class Windspeed {
+import processing.core.PApplet;
+
+public class Windspeed extends PowerGeneration{
+
+    Windspeed(int updateRate, String table, String columnP, String columnE, PApplet p){
+        this.p=p;
+        this.updateRate=updateRate;
+        this.table=table;
+        this.columnP=columnP;
+        this.columnE=columnE;
+    }
 }
