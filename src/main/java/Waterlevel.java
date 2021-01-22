5d154df21a2309e118a3bee8b982bfd48fa2da63
katrinesondrup <katrinesondrup@gmail.com>
2021-01-22 08:48:35 +0100

    hej

--- src/main/java/Waterlevel.java
+++ src/main/java/Waterlevel.java
@@ -1,2 +1,13 @@
-public class Waterlevel {
+import processing.core.PApplet;
+
+public class Waterlevel extends PowerGeneration{
+
+    Waterlevel(int updateRate, String table, String columnP, String columnE, PApplet p){
+        super(updateRate, table, columnP, columnE, p);
+        this.p=p;
+        this.updateRate=updateRate;
+        this.table=table;
+        this.columnP=columnP;
+        this.columnE=columnE;
+    }
 }
