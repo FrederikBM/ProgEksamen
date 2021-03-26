6d83a14045365c2b9da6486bb6f3f2c7a3e94e0a
Lukrial <54103585+Lukrial@users.noreply.github.com>
2021-03-26 10:08:01 +0100

    uh

--- src/main/java/LightsOut.java
+++ src/main/java/LightsOut.java
@@ -1,4 +1,12 @@
-public class LightsOut {
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class LightsOut extends Main{
     //tænd lyset: ingen risiko for død ved ulve. Der trækkes den normale 10 strøm i sekundet
     //sluk lyset: 10 sekunder før død. der trækkes nu kun 5 strøm fra i sekundet.
+    PApplet p;
+
+    public LightsOut(){
+        this.p = p;
+    }
 }
